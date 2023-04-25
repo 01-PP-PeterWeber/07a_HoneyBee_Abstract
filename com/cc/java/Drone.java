00e67d77d3207b57e9b33ba7cb542a558abe0121
Peter Weber <peter-aalen@gmx.net>
2023-04-25 12:52:36 +0200

    HoneyBee Abstract

--- com/cc/java/Drone.java
+++ com/cc/java/Drone.java
@@ -2,9 +2,16 @@ package com.cc.java;
 
 public class Drone extends HoneyBee{
     
+    @Override
     public String doYourJob() {
-        return "I'm a drone, I'm going to date our Queen!";
+        return "I'm a drone, I'm going to fuck our Queen!";
     }
 
+    @Override
+    public String fly() {
+        return "I can fuck at 10mph! jihaaaaaaaaaaaaaaaa";
+    }
+
+    
 
 }
